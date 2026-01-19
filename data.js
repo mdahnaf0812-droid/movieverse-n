bd39389a2913c2ff6521d9df14de065473993d72
mdahnaf0812-droid <mdahnaf0812@gmail.com>
2026-01-19 10:52:29 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    title: "Avengers",
+    rating: "4.5",
+    category: "Movie",
+    img: "https://m.media-amazon.com/images/I/81ExhpBEbHL._AC_SL1500_.jpg",
+    desc: "Earth's mightiest heroes must come together to save the world.",
+    trailer: "https://www.youtube.com/watch?v=eOrNdBpGMv8"
+  },
+  {
+    title: "Spider-Man",
+    rating: "4.7",
+    category: "Movie",
+    img: "https://m.media-amazon.com/images/I/71niXI3lxlL._AC_SL1024_.jpg",
+    desc: "A young hero learns responsibility with great power.",
+    trailer: "https://www.youtube.com/watch?v=t06RUxPbp_c"
+  },
+  {
+    title: "Naruto",
+    rating: "4.8",
+    category: "Anime",
+    img: "https://upload.wikimedia.org/wikipedia/en/9/94/NarutoCoverTankobon1.jpg",
+    desc: "A ninja who dreams of becoming Hokage.",
+    trailer: "https://www.youtube.com/watch?v=-G9BqkgZXRA"
+  }
+];
