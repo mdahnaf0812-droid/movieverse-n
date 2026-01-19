e92efda77d34559dbb42b4ebff95485f26a80ab3
mdahnaf0812-droid <mdahnaf0812@gmail.com>
2026-01-19 11:39:25 +0600

    Update data.js

--- data.js
+++ data.js
@@ -1,18 +1,26 @@
 const movies = [
   {
     title: "Spider-Man",
-    poster: "https://i.imgur.com/3ZQ3Z6B.jpg",
+    poster: "https://i.imgur.com/3jLPB46.jpg",
     rating: "4.7",
     category: "Movie",
     desc: "A young hero learns responsibility with great power.",
     trailer: "https://www.youtube.com/watch?v=t06RUxPbp_c"
   },
   {
-    title: "Avengers",
-    poster: "https://i.imgur.com/e1hLQ2A.jpg",
-    rating: "4.8",
+    title: "Avengers Endgame",
+    poster: "https://i.imgur.com/e1hLQ2O.jpg",
+    rating: "4.9",
     category: "Movie",
-    desc: "Earth’s mightiest heroes must unite.",
+    desc: "Earth’s mightiest heroes unite for one final battle.",
     trailer: "https://www.youtube.com/watch?v=TcMBFSGVi1c"
+  },
+  {
+    title: "Naruto",
+    poster: "https://i.imgur.com/6X4F6ZQ.jpg",
+    rating: "4.8",
+    category: "Anime",
+    desc: "A ninja’s journey to become Hokage.",
+    trailer: "https://www.youtube.com/watch?v=-G9BqkgZXRA"
   }
 ];
