50d4eadcf924317a754d07f33dbd7f9491f5b07e
mdahnaf0812-droid <mdahnaf0812@gmail.com>
2026-01-19 11:59:03 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
-const movies = [
-  {
-    title: "Spider-Man",
-    poster: "https://i.imgur.com/8UG2ZQp.jpg",
-    rating: "4.7"
-  },
-  {
-    title: "Avengers Endgame",
-    poster: "https://i.imgur.com/e1hLQ2k.jpg",
-    rating: "4.8"
+document.addEventListener("DOMContentLoaded", () => {
+
+  const movies = [
+    {
+      title: "Spider-Man",
+      poster: "https://i.imgur.com/8UG2ZQp.jpg",
+      rating: "4.7"
+    },
+    {
+      title: "Avengers Endgame",
+      poster: "https://i.imgur.com/e1hLQ2k.jpg",
+      rating: "4.8"
+    }
+  ];
+
+  const movieList = document.getElementById("movieList");
+
+  if (!movieList) {
+    console.error("movieList div not found");
+    return;
   }
-];
 
-const movieList = document.getElementById("movieList");
-
-if(movieList){
   movieList.innerHTML = "";
+
   movies.forEach(movie => {
-    movieList.innerHTML += `
-      <div class="card">
-        <img src="${movie.poster}">
-        <div class="overlay">
-          <h3>${movie.title}</h3>
-          <span>⭐ ${movie.rating}</span>
-        </div>
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+      <img src="${movie.poster}" alt="${movie.title}">
+      <div class="overlay">
+        <h3>${movie.title}</h3>
+        <span>⭐ ${movie.rating}</span>
       </div>
     `;
+
+    movieList.appendChild(card);
   });
-}else{
-  console.error("movieList div not found");
-}
+
+});
