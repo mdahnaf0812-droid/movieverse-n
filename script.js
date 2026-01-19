73505a652f9586b833f8c70d6d2a9084851c33ae
mdahnaf0812-droid <mdahnaf0812@gmail.com>
2026-01-19 12:05:42 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,30 @@
-// ===== MOVIE DATA =====
+const movieList = document.getElementById("movieList");
+
+console.log("JS Loaded", movieList);
+
 const movies = [
   {
-    title: "Avengers",
-    poster: "https://i.imgur.com/8UG2KQx.jpg",
-    rating: "⭐ 4.5",
-    category: "Movie"
+    title: "Avengers Endgame",
+    rating: "⭐ 4.8",
+    image: "https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg"
   },
   {
     title: "Spider-Man",
-    poster: "https://i.imgur.com/3jLPB46.jpg",
     rating: "⭐ 4.7",
-    category: "Movie"
-  },
-  {
-    title: "Batman",
-    poster: "https://i.imgur.com/6Iej2c3.jpg",
-    rating: "⭐ 4.6",
-    category: "Movie"
-  },
-  {
-    title: "Naruto",
-    poster: "https://i.imgur.com/OVx1K3F.jpg",
-    rating: "⭐ 4.8",
-    category: "Anime"
+    image: "https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg"
   }
 ];
 
-// ===== RENDER MOVIES =====
-const movieList = document.getElementById("movieList");
+if (!movieList) {
+  alert("movieList NOT FOUND");
+}
 
 movies.forEach(movie => {
   const card = document.createElement("div");
   card.className = "card";
 
   card.innerHTML = `
-    <img src="${movie.poster}" alt="${movie.title}">
+    <img src="${movie.image}">
     <div class="overlay">
       <h3>${movie.title}</h3>
       <span>${movie.rating}</span>
